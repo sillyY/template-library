20c104c9c50db398d7ce3871419466a8d0541863
sillyy <965768042@qq.com>
2019-10-21 16:06:28 +0800

    fix valiEmail

--- JavaScript/validate/email.js
+++ JavaScript/validate/email.js
@@ -4,7 +4,7 @@ export const valiPhone = (value, callback) => {
   } else if (
     !/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(value)
   ) {
-    callback(new Error('请输入正确的手机号'))
+    callback(new Error('请输入正确的邮箱'))
   } else {
     callback()
   }
